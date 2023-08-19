70ea6c41a27130d056c5864ae22888d54aeb8f36
Kathryn Zamiela <32081918+catomancer@users.noreply.github.com>
2023-08-19 18:33:16 -0500

    Added basic portfolio React structure, need to finish populating.

---
+++ src/components/Contact.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+
+export default function Contact() {
+    const [state, handleSubmit] = useForm("xgejkvnb");
+    if (state.succeeded) {
+        return <p>Thank you for your contacting me!  I will get back to you shortly.</p>; //FIGURE OUT CLASS FOR THIS
+    }
+    return (
+        <section id="contact" className="relative">
+            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+                <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        className="absolute inset-0"
+                        title="map"
+                        frameBorder={0}
+                        marginHeight={0}
+                        marginWidth={0}
+                        style={{ filter: "opactiy(0.7)" }}
+                        src="https://www.openstreetmap.org/export/embed.html?bbox=-122.66819000244142%2C47.154003588277526%2C-122.32795715332033%2C47.33184253461828&amp;layer=mapnik"
+                    />
+                </div>
+                <form name="contact" onSubmit={handleSubmit} className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">Contact Me</h2>
+                    <p className="leading-relaxed mb-5">Description</p>
+                    <div className="relative mb-4">
+                        <label htmlFor="name" className="leading-7 text-sm text-gray-400">Name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                         />
+                    </div>
+                    <div className="relative mb-4">
+                        <label
+                            htmlFor="email"
+                            className="leading-7 text-sm text-gray-400">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label
+                            htmlFor="message"
+                            className="leading-7 text-sm text-gray-400">
+                            Message
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        disabled={state.submitting}
+                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                            Submit
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
+}
